b6696a5be5a4202dd1f9fb561855dd18a0b0da3d
HishamT <hisham.taweel@gmail.com>
2024-09-02 12:54:35 +0200

    Test

--- src/main/java/com/shortenit/app/views/WelcomeView.java
+++ src/main/java/com/shortenit/app/views/WelcomeView.java
@@ -1,21 +1,22 @@
 package com.shortenit.app.views;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.component.notification.Notification;
 
 @Route("welcome")
 public class WelcomeView extends VerticalLayout {
 
-    public WelcomeView() {
-        TextField nameField = new TextField("Your name");
-        Button sayHelloButton = new Button("Say hello");
-        sayHelloButton.addClickListener(e -> {
-            Notification.show("Welcome to Shorten It, " + nameField.getValue());
+  public WelcomeView() {
+    TextField nameField = new TextField("Your name");
+    Button sayHelloButton = new Button("Say hello");
+    sayHelloButton.addClickListener(
+        e -> {
+          Notification.show("Welcome to Shorten It, " + nameField.getValue());
         });
 
-        add(nameField, sayHelloButton);
-    }
+    add(nameField, sayHelloButton);
+  }
 }
